094fb0267a37d1f90379f992bab27debddb588b3
keshav saini <sainikeshav241@gmail.com>
2026-04-01 00:32:00 +0530

    add cookie-parser and cors middleware; implement async error handling and response classes

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,19 @@
+class ApiResponse {
+    constructor(statusCode , message = "success" , data ){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
+
+/*
+server status code
+
+Informational responses (100 – 199)
+Successful responses (200 – 299)
+Redirection messages (300 – 399)
+Client error responses (400 – 499)
+Server error responses (500 – 599)
+*/
