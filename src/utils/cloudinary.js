afeb41e7f1ad6b7d706472ad798fcbc18fe91265
keshav saini <sainikeshav241@gmail.com>
2026-04-06 15:30:44 +0530

    add multer and cloudinary integration for file uploads; create LEARNING5.md for file handling and cloud storage documentation

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+
+// this configuration allows us to upload files to cloudinary and get the url of the uploaded file
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) {
+            return null
+        }
+        // Upload the file to Cloudinary
+        const result = await cloudinary.uploader.upload(localFilePath, {resource_type: 'auto'})
+        console.log("file is uploaded on cloudinary", result.url)
+        return result
+    }catch (error) {
+        fs.unlinkSync(localFilePath)// remove the file from local storage if there is an error while uploading to cloudinary
+        return null
+    }
+}
